9c4fc5ab692ce06bbaeaa978f5e10e3e0ef72d59
fazeel310 <160825784+fazeel310@users.noreply.github.com>
2024-02-23 13:13:15 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
 // This is a simple Java program.
 // FileName : "HelloWorld.java".
  
-public class helloworld {
+public class Main {
     public static void main(String[] args) {
     System.out.println("Hello!!!!!");
-     System.out.println("How are you?")
+     System.out.println("How are you?");
     }
 }  
