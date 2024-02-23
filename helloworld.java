1b52f9913acdfe5493633149a437b3e7731b1b2c
fazeel310 <160825784+fazeel310@users.noreply.github.com>
2024-02-23 12:50:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 // FileName : "HelloWorld.java".
  
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
     System.out.println("Hello!!!!!");
      System.out.println("How are you?")
     }
