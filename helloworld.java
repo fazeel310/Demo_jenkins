f95e1ee21b32d8d6b1c29b214ad41e4871ead12a
fazeel310 <160825784+fazeel310@users.noreply.github.com>
2024-03-01 11:36:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@
  
 public class helloworld {
     public static void main(String[] args) {
-    System.out.println("Hello!");
+    System.out.println("Hello!") ;
     System.out.println("How are you?");
     }
 }  
